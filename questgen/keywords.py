52988a7386a94b996582a2807c8a44424d61a30f
Nishkrit Desai <nishkritdesai10@gmail.com>
2018-08-25 15:30:52 -0700

    Changed API auth method

--- questgen/keywords.py
+++ questgen/keywords.py
@@ -8,10 +8,14 @@ Author: ndesai
 """
 
 import indicoio
-import os
 
 
 def get_keywords(data):
     """Returns all keywords in an article along with a confidence score """
-    indicoio.config.api_key = os.environ["INDICO_API_KEY"]
+    set_api_key('api_key.txt')
     return indicoio.keywords(data)
+
+
+def set_api_key(api_file):
+    api_key = open(api_file, 'r').read()
+    indicoio.config.api_key = api_key
